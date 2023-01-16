71ef4faf63a717b7b8a1c19bb7042360d8921495
Roshni V R <vrroshni2001@gmail.com>
2023-01-16 11:03:39 +0530

    Completed home section

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import Roshni from '../assets/roshnivr/roshni.jpg';
+
+const Home = () => {
+    return (
+        <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800' >
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+                <div className='flex flex-col justify-center h-full'>
+                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Python Full Stack Developer</h2>
+                    <p className='text-gray-500 py-4 max-w-md'>self-taught Python FullStack Developer with a passion for programming languages and exploring
+                        technological advances. Possess excellent communication skills and have an eye for detail. In order to
+                        expand and utilize my learning, skills and knowledge, I am seeking a challenging position in a reputable
+                        organization.</p>
+                    <div>
+                        <button className='group text-white px-6 py-3 my-2 w-fit flex items-center rounded-md bg-gradient-to-r  from-cyan-500 to-blue-500 cursor-pointer'>
+                            PortFolio <span className='group-hover:rotate-90 duration-200'><MdOutlineKeyboardArrowRight size={25} className="ml-1" /></span>
+                        </button>
+                    </div>
+                </div>
+                <div>
+                   <img src={Roshni} alt="Roshni V R"  className='rounded-2xl mx-auto w-2/3 md:w-full'/> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
