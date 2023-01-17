354e1e582ed071be4d105f0351949f4ce37c4cbf
Roshni V R <vrroshni2001@gmail.com>
2023-01-17 15:52:36 +0530

    completed about section

---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt illo accusantium nihil nobis! Quam cupiditate quisquam deleniti tempore explicabo quibusdam! Animi praesentium recusandae nostrum cupiditate odio tempore asperiores porro ut eum. Sunt enim odit, iste in labore deleniti, repudiandae quam ducimus consequuntur hic quos ad modi qui quidem rerum blanditiis laudantium laboriosam possimus, exercitationem placeat tempore. Ullam fuga dolores aperiam maxime sint, iure iste, mollitia accusamus quas asperiores magnam distinctio veniam corrupti assumenda id incidunt ex nulla iusto autem obcaecati.
+                </p>
+                <br />
+                <p className='text-xl' >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ea saepe nesciunt quibusdam, officia dolorem quam! Vitae, illum natus pariatur, illo minus ad nobis repellendus fugiat placeat tenetur sequi aliquam voluptatem? Eligendi porro dolore quibusdam ipsam alias nisi accusamus animi sequi placeat similique minima reiciendis nulla odio, quos atque minus error vitae illo officia. Nostrum nam, ullam optio similique inventore minima vel debitis? Omnis rem voluptatem repudiandae magnam dolore voluptate fugit sapiente qui ipsam, nemo assumenda iure dolorum quisquam? Hic commodi excepturi rerum, pariatur velit eum repellendus sapiente libero corrupti voluptate temporibus perferendis aspernatur possimus modi repudiandae asperiores error a!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -42,7 +42,7 @@ const NavBar = () => {
             </div>
             {nav && <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
                 {links.map(({ link, id }) => (
-                    <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl hover:scale-105 duration-200'>{link}</li>
+                    <li key={link} className='px-4 cursor-pointer capitalize py-6 text-4xl hover:scale-105 duration-200'>{link}</li>
                 ))}
             </ul>}
 
